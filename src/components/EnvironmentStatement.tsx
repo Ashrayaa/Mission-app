639acd3848f468609dc67bf089aebaafe6460b0a
Ashrayaa <mailtoashrayaa@gmail.com>
2023-04-21 12:37:15 +0530

    faq ADDED

--- src/components/EnvironmentStatement.tsx
+++ src/components/EnvironmentStatement.tsx
@@ -1,11 +1,11 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 import Proofofstake from "public/proof.svg";
 import Cloud from "public/cloud.svg";
 import Framethree from "public/frame3.svg";
 import Nodearmy from "public/nodearmy.svg";
 
-type Props = {}
+type Props = {};
 
 const EnvironmentStatement = (props: Props) => {
   return (
@@ -13,27 +13,31 @@ const EnvironmentStatement = (props: Props) => {
       <div className="p-6">
         <h1 className="text-5xl font-extrabold my-6">ENVIRONMENT STATEMENT</h1>
         <div className="flex flex-col gap-8">
-
           {/* 1.proof of stake */}
           <div className="flex flex-col gap-4">
-            <h2 className="text-2xl font-bold">CO-NODES</h2>
+            <h2 className="text-2xl font-bold">PROOF OF STAKE</h2>
             <p className="text-[#b6b6b6]">
-              Collaborative staking among users to pool to achieve threshold
-              stake and operate collectively. This democratises participation
-              and allows sub-stake holders to benefit from reward protocols. It
-              also enables compounding as participants can sweep earned rewards
-              into further Co-Nodes
+              Proof of stake blockchain validation is more environmentally
+              friendly than proof of work, as it consumes significantly less
+              energy by not requiring computational puzzles to be solved.
+              Validators are selected based on cryptocurrency holdings, reducing
+              the carbon footprint of the network. Unless directly disclosed
+              none of the projects we support will operate through Proof of
+              Work.
             </p>
             <Image src={Proofofstake} alt={"Proofofstake"} />
           </div>
 
           {/*2 Cloud computing  */}
           <div className="flex flex-col gap-4">
-            <h2 className="text-2xl font-bold">MANAGED NODES</h2>
+            <h2 className="text-2xl font-bold">CLOUD COMPUTING</h2>
             <p className="text-[#b6b6b6]">
-              Outsource node deployment, management, and maintenance to Node
-              Army for a small commission of earned rewards. Develop a passive
-              yield portfolio with minimum time and skill commitment.
+              Cloud computing provides more efficient resource usage , reducing
+              the need for physical hardware and lowering the carbon footprint
+              of the network. Other benefits include increased scalability,
+              faster transaction speeds, and improved accessibility. Overall,
+              cloud computing provides an eco-friendly and efficient solution
+              for validating and recording transactions on PoS blockchains.
             </p>
             <Image src={Cloud} alt={"cloud"} />
           </div>
@@ -42,21 +46,20 @@ const EnvironmentStatement = (props: Props) => {
 
           {/* 3 Node army*/}
           <div className="flex flex-col gap-4">
-            <h2 className="text-2xl font-bold">NODES AS A SERVICE</h2>
+            <h2 className="text-2xl font-bold">NODES ARMY</h2>
             <p className="text-[#b6b6b6]">
-              An industrial node solution for projects requiring robust
-              decentralised node infrastructures at scale. Node Army
-              collaborates with projects during development phase and through a
-              blockchain lifecycle.
+              We regularly review cloud computing platforms to ensure you can
+              choose from proven, reliable, scalable and environmentally
+              responsible providers. Node Army will endeavor to integrate a
+              energy utilization metric to our and your dashboards in the near
+              future for transparency of impact
             </p>
             <Image src={Nodearmy} alt={"Nodearmy"} />
           </div>
-
-       
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default EnvironmentStatement
+export default EnvironmentStatement;
--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,11 +1,178 @@
-import React from 'react'
+import React from "react";
+import { useState } from "react";
 
-type Props = {}
-
-const FAQ = (props: Props) => {
-  return (
-    <div>FAQ</div>
-  )
+interface FAQparameters {
+  question: string;
+  answer: string;
+  isVisible: boolean;
+  setIsVisible: (visible: boolean) => void;
 }
 
-export default FAQ
+const Section = ({
+  question,
+  answer,
+  isVisible,
+  setIsVisible,
+}: FAQparameters) => {
+  return (
+    <div className="flex flex-col gap-3 justify-center mx-2 xl:mx-48 2xl:mx-64 rounded-xl py-2">
+      <div className="flex justify-between gap-8">
+        <h2 className=" md:text-left xl:text-lg xl:leading-9">{question}</h2>
+        {isVisible ? (
+          <button
+            onClick={() => {
+              setIsVisible(false);
+            }}
+          >
+             <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 4.5v15m7.5-7.5h-15"
+              />
+            </svg> 
+          </button>
+        ) : (
+          <button
+            onClick={() => {
+              setIsVisible(true);
+            }}
+          >
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="w-6 h-6"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M19.5 12h-15"
+              />
+            </svg>
+          
+          </button>
+        )}
+      </div>
+      <div
+        className={`max-h-0 ${
+          isVisible ? "max-h-[300px]" : ""
+        } overflow-hidden transition-all duration-700`}
+      >
+        <p className="text-[#C0C0C8] text-sm md:text-left xl:text-lg xl:leading-9">
+          {answer}
+        </p>
+      </div>
+    </div>
+  );
+};
+
+const FAQ = () => {
+  const [visibleSection, setVisibleSection] = useState("");
+
+  return (
+    <section className="text-white p-5">
+      <div className="flex flex-col gap-6 justify-center items-center">
+        <h1 className="text-5xl font-extrabold my-6">FREQUENTLY ASKED</h1>
+        <div className="flex flex-col gap-3">
+          <Section
+            question={"WHAT IS NODE ARMY?"}
+            answer={
+              "Node Army is a cloud-native blockchain infrastructure provider that empowers individuals and businesses to deploy and manage nodes, even without technical expertise. Our infrastructure is secure and scalable, with advanced features such as co-nodes, compounding, and managed nodes."
+            }
+            isVisible={visibleSection == "One"}
+            setIsVisible={(reveal) => {
+              if (reveal) {
+                setVisibleSection("One");
+              } else {
+                setVisibleSection("");
+              }
+            }}
+          />
+          <Section
+            question={"HOW CAN I BENEFIT FROM USING NODE ARMY"}
+            answer={
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            }
+            isVisible={visibleSection == "Two"}
+            setIsVisible={(reveal) => {
+              if (reveal) {
+                setVisibleSection("Two");
+              } else {
+                setVisibleSection("");
+              }
+            }}
+          />
+
+          <Section
+            question={"DO I NEED TECHNICAL EXPERTISE TO USE NODE ARMY?"}
+            answer={
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            }
+            isVisible={visibleSection == "Three"}
+            setIsVisible={(reveal) => {
+              if (reveal) {
+                setVisibleSection("Three");
+              } else {
+                setVisibleSection("");
+              }
+            }}
+          />
+
+          <Section
+            question={"WHAT KIND OF SUPPORT DOES NODE ARMY PROVIDE?"}
+            answer={
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            }
+            isVisible={visibleSection == "Four"}
+            setIsVisible={(reveal) => {
+              if (reveal) {
+                setVisibleSection("Four");
+              } else {
+                setVisibleSection("");
+              }
+            }}
+          />
+          <Section
+            question={"HOW DO I GET STARTED WITH NODE ARMY?"}
+            answer={
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            }
+            isVisible={visibleSection == "Five"}
+            setIsVisible={(reveal) => {
+              if (reveal) {
+                setVisibleSection("Five");
+              } else {
+                setVisibleSection("");
+              }
+            }}
+          />
+          <Section
+            question={"HOW WILL I BE REWARDED BY USING NODE ARMY?"}
+            answer={
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
+            }
+            isVisible={visibleSection == "Six"}
+            setIsVisible={(reveal) => {
+              if (reveal) {
+                setVisibleSection("Six");
+              } else {
+                setVisibleSection("");
+              }
+            }}
+          />
+        </div>
+      </div>
+    </section>
+  );
+};
+export default FAQ;
